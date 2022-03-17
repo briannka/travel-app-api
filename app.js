8c4c7bfc9c2d9018d77056dcfa2809e98c279755
briannailiev <brianna.iliev@sumup.com>
2022-03-17 20:32:44 +0200

    Added new path

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require("express")
 const app = express()
 
-// use the express-static middleware
-app.use(express.static("public"))
-
 // define the first route
-app.get("/", function (req, res) {
+app.get("/api", function (req, res) {
   res.send("<h1>Hello World!</h1>")
 })
 
